ed8ab306d878be0edfb4a5c32ceb0035eacea3f3
mgiftson0 <mgiftson00@gmail.com>
2025-01-23 22:37:12 +0000

    the header has been made sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,34 +2,37 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center px-6 py-4 text-white">
+    <header className="sticky top-0 z-50 flex justify-between items-center px-6 py-4 text-white">
       {/* Logo and Name */}
       <div className="ml-4 flex items-center space-x-2">
         <img src={logo} alt="Logo" className="h-8 w-8" />
-        <span className="text-xl font-bold"  style={{ fontFamily: 'Comic Sans MS, Comic Sans, cursive' }}>
+        <span
+          className="text-xl font-bold"
+          style={{ fontFamily: 'Comic Sans MS, Comic Sans, cursive' }}
+        >
           futuride
         </span>
       </div>
 
       {/* Navigation Links */}
       <nav className="hidden md:flex space-x-6 mr-20">
-        <a href="#home" className="hover:text-gray-500">
+        <a href="#home" className="hover:text-gray-500 cursor-pointer">
           Home
         </a>
-        <a href="#cars" className="hover:text-gray-500">
+        <a href="#cars" className="hover:text-gray-500 cursor-pointer">
           Cars
         </a>
-        <a href="#about" className="hover:text-gray-500">
+        <a href="#about" className="hover:text-gray-500 cursor-pointer">
           About
         </a>
-        <a href="#deals" className="hover:text-gray-500">
+        <a href="#deals" className="hover:text-gray-500 cursor-pointer">
           Deals
         </a>
       </nav>
 
       {/* Mobile Menu Icon */}
       <div className="md:hidden">
-        <button className="text-white">☰</button>
+        <button className="text-white cursor-pointer">☰</button>
       </div>
     </header>
   );
